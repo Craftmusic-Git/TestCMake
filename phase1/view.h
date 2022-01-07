4e56b64f66ce67dee83d1eb66637ddc210a1d9f0
hugo <hugo.baudry@uha.fr>
2022-01-07 14:54:23 +0100

    ajout des classes human et view

---
+++ phase1/view.h
@@ -0,0 +1,17 @@
+//
+// Created by hugo-baudry on 07/01/2022.
+//
+
+#ifndef TPADEQLOGMAT_VIEW_H
+#define TPADEQLOGMAT_VIEW_H
+
+#include "board.h"
+
+class view {
+
+    void display(board&);
+ 
+};
+
+
+#endif //TPADEQLOGMAT_VIEW_H
