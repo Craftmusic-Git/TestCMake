d1579d60a2d529a2b1cee3b09f48732df5883038
hugo <hugo.baudry@uha.fr>
2022-01-07 17:24:11 +0100

    ia.c qui fonctionne

--- phase2/ialib/src/ia.c
+++ phase2/ialib/src/ia.c
@@ -1,5 +1,16 @@
 #include "../include/ia.h"
 
 void iaplay(int grid[3][3], int* l, int* c){
-
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (grid[i][j] == 0)
+            {
+                *l = i;
+                *c = j;
+                return;
+            }
+        }
+    }
 }
