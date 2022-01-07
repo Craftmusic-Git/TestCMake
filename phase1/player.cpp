c7b250348422a4e8851b66b4177ac31234cb03ac
hugo <hugo.baudry@uha.fr>
2022-01-07 14:26:52 +0100

    ajout de la classe player

---
+++ phase1/player.cpp
@@ -0,0 +1,16 @@
+//
+// Created by hugo-baudry on 07/01/2022.
+//
+
+#include "board.h"
+#include "player.h"
+
+player::player() {
+
+}
+
+void player::player(board& b){
+    
+}
+
+}
---
+++ phase1/player.h
@@ -0,0 +1,21 @@
+//
+// Created by hugo-baudry on 07/01/2022.
+//
+
+#ifndef TPADEQLOGMAT_PLAYER_H
+#define TPADEQLOGMAT_PLAYER_H
+
+#include "board.h"
+
+
+class player {
+ 
+player();
+
+void play(board&);
+
+ 
+};
+
+
+#endif //TPADEQLOGMAT_PLAYER_H
